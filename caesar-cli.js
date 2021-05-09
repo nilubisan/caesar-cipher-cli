6af33ded0b11e13dcc471dce2f5786aaade0e80f
Rinat <r.nas9329@gmail.com>
2021-05-09 20:22:02 +0600

    feat: add basic cli

---
+++ caesar-cli.js
@@ -0,0 +1,28 @@
+const ALLOWED_ACTIONS = ['encode', 'decode'];
+const commander = require('commander');
+const program = new commander.Command();
+program
+.option(
+    '-s, --shift <type>', 'a shift', ''
+)
+.option(
+    '-i, --input <type>', 'an input file', ''
+)
+.option(
+    '-o, --output <type>', 'an output file', '', 
+)
+.option(
+    '-a, --action <type>', 'an action encode/decode', '', 
+);
+program.parse(process.argv);
+const options = program.opts();
+
+if(!options.shift || !options.action) {
+    console.log(`Error! Required option ${!options.shift ? '\'shift\'' : '\'action\''} is missed. The program has been aborted`);
+}
+if (!ALLOWED_ACTIONS.includes(options.action)) {
+    console.log (`Action ${options.action} is not allowed. Use one of these instead: ${ALLOWED_ACTIONS.reduce((_, action) => `${_} ${action}`, '')}`);
+}
+if(!Number.isInteger(+options.shift)) {
+    console.log('Shift error!');
+}
